2b8bac18c50b13cb2992e2e5399d352214478ebb
ganesha87 <ganeshvarma.cse@gmail.com>
2023-03-06 15:47:33 +0530

    updated path

--- scripts/getlatestTFState.py
+++ scripts/getlatestTFState.py
@@ -1,6 +1,5 @@
 import boto3
 
+bucketName = 'tf-states-demo'
 s3 = boto3.resource('s3')
-#s3://tf-states-demo/assignments/terraform.tfstate
-s3.meta.client.download_file('tf-states-demo', 'assignments/terraform.tfstate', './config/assgn.tfstate')
-s3.meta.client.download_file('tf-states-demo', 'permissionsets/terraform.tfstate', './config/psets.tfstate')
+s3.meta.client.download_file(bucketName, 'permissionsets/terraform.tfstate', './terraform/terraform.tfstate')
