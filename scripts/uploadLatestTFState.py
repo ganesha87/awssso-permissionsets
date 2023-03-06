259f55fbcf7ccbf62df65440d953d557ff71619f
ganesha87 <ganeshvarma.cse@gmail.com>
2023-03-06 15:48:27 +0530

    Update uploadLatestTFState.py

--- scripts/uploadLatestTFState.py
+++ scripts/uploadLatestTFState.py
@@ -1,5 +1,5 @@
 import boto3
 
+bucketName = 'tf-states-demo'
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file('../tf-assignments/terraform.tfstate','tf-states-demo', 'assignments/terraform.tfstate')
-s3.meta.client.upload_file('../tf-assignments/terraform.tfstate','tf-states-demo', 'permissionsets/terraform.tfstate')
+s3.meta.client.upload_file('./terraform/terraform.tfstate',bucketName, 'permissionsets/terraform.tfstate')
